38cf97c2b0c13888240517e10f51e86db0487678
erivaldomalebo <erivaldomalebo2206@gmail.com>
2026-07-23 19:26:52 +0100

    fix(ui): add 'use client' in button component

--- apps/web/src/app/(public)/page.tsx
+++ apps/web/src/app/(public)/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@workspace/ui/components/button";
+
 export default function Home() {
-	return <div>Hello world</div>;
+	return (
+		<div>
+			Hello world
+			<Button>Gerar frase</Button>
+		</div>
+	);
 }
